f4e34c96debbd194191cb9aaad274167ad654811
chrishoen <chris.hoen@red-inc.us>
2016-07-15 15:24:55 -0400

    moe ThreeVector

--- ThreeVector/Source/CmdLineExec.cpp
+++ ThreeVector/Source/CmdLineExec.cpp
@@ -51,224 +51,47 @@ void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
 
 void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
 {
-   double tT[3][3] = {{11.0,12.0,13.0},
-                      {21.0,22.0,23.0},
-                      {31.0,32.0,33.0}};
-   Dsp::ThreeMatrix tX;
-   tX.initialize(3,3,&tT[0][0]);
-
-   tX.show("tX");
 }
 
 //******************************************************************************
 
 void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
 {
-   // X1
-   double tT1[3] = {101.0,
-                    102.0,
-                    103.0};
-
-   Dsp::ThreeVector tX1;
-   tX1.initialize(3,&tT1[0]);
-
-   tX1.show("tX1");
-
-   // X2
-   Dsp::ThreeVector tX2(tX1);
-
-   tX2.show("tX2");
-
-   // X3
-   Dsp::ThreeVector tX3 = tX1;
-
-   tX3.show("tX3");
-
-   // X4
-   Dsp::ThreeVector tX4;
-   tX4 = tX1;
-
-   tX4.show("tX4");
-
-   // X5
-   Dsp::ThreeVector tX5;
-   tX5.initialize(3);
-   tX5 = tX1;
-
-   tX5.show("tX5");
 }
 
 //******************************************************************************
 
 void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
 {
-   // X1
-   double tT1[3] = {101.0,
-                    102.0,
-                    103.0};
-
-   Dsp::ThreeVector tX1;
-   tX1.initialize(3,&tT1[0]);
-
-   tX1.show("tX1");
-
-   // X2
-   double tT2[3] = {201.0,
-                    202.0,
-                    203.0};
-
-   Dsp::ThreeVector tX2;
-   tX2.initialize(3,&tT2[0]);
-
-   tX2.show("tX2");
-
-   // X3
-   Dsp::ThreeVector tX3(3);
-
-   tX3 = tX1 + tX2;
-
-   tX3.show("tX3");
-
-   tX3 = tX1 + tX2;
-
-   tX3.show("tX3");
 }
 
 //******************************************************************************
 
 void CmdLineExec::executeGo5(Ris::CmdLineCmd* aCmd)
 {
-   // X1
-   double tT1[3] = {101.0,
-                    102.0,
-                    103.0};
-
-   Dsp::ThreeVector tX1;
-   tX1.initialize(3,&tT1[0]);
-
-   tX1.show("tX1");
-
-   // X2
-   double tT2[3] = {201.0,
-                    202.0,
-                    203.0};
-
-   Dsp::ThreeVector tX2;
-   tX2.initialize(3,&tT2[0]);
-
-   tX2.show("tX2");
-
-   // X3
-   Dsp::ThreeVector tX3(3);
-
-   add(tX3,tX1,tX2);
-
-   tX3.show("tX3");
-
 }
 
 //******************************************************************************
 
 void CmdLineExec::executeGo6(Ris::CmdLineCmd* aCmd)
 {
-   double tAV[3][3] = {{11.0,12.0,13.0},
-                       {21.0,22.0,23.0},
-                       {31.0,32.0,33.0}};
-   Dsp::ThreeMatrix tA;
-   tA.initialize(3,3,&tAV[0][0]);
-   tA.show("tA");
-
-   // X1
-   double tT1[3] = {101.0,
-                    102.0,
-                    103.0};
-
-   Dsp::ThreeVector tX1;
-   tX1.initialize(3,&tT1[0]);
-
-   tX1.show("tX1");
-
-   // X2
-   Dsp::ThreeVector tX2(3);
-
-   multiply(tX2,tA,tX1);
-
-   tX2.show("tX2");
-
 }
 
 //******************************************************************************
 
 void CmdLineExec::executeGo7(Ris::CmdLineCmd* aCmd)
 {
-   double tAV[2][2] = {{1.0,0.0},
-                       {0.0,2.0}};
-   Dsp::ThreeMatrix tA;
-   tA.initialize(2,2,&tAV[0][0]);
-   tA.show("tA");
-
-   // X1
-   double tT1[2] = {101.0,
-                    102.0};
-
-   Dsp::ThreeVector tX1;
-   tX1.initialize(2,&tT1[0]);
-   tX1.show("tX1");
-
-   // X2
-   Dsp::ThreeVector tX2(2);
-
-   multiply(tX2,tA,tX1);
-
-   tX2.show("tX2");
 }
 
 //******************************************************************************
 
 void CmdLineExec::executeGo8(Ris::CmdLineCmd* aCmd)
 {
-   aCmd->setArgDefault(1,45.0);
-   double tAngle = aCmd->argDouble(1);
-
-   Dsp::ThreeMatrix tL;
-   tL.initialize(3,3);
-   tL.setRotateX(tAngle);
-   
-   tL.show("tL");
 }
 
 //******************************************************************************
 
 void CmdLineExec::executeGo9(Ris::CmdLineCmd* aCmd)
 {
-   aCmd->setArgDefault(1,45.0);
-   aCmd->setArgDefault(0, 0.0);
-   double tRollAngle  = aCmd->argDouble(1);
-   double tPitchAngle = aCmd->argDouble(2);
-
-   Dsp::ThreeMatrix tRy;
-   tRy.initialize(3,3);
-   tRy.setRotateY(tRollAngle);
-   
-   Dsp::ThreeMatrix tRx;
-   tRx.initialize(3,3);
-   tRx.setRotateX(tPitchAngle);
-   
-   Dsp::ThreeMatrix tL01;
-   tL01.initialize(3,3);
-   multiply(tL01,tRx,tRy);
-   
-   Dsp::ThreeMatrix tL10;
-   tL10.initialize(3,3);
-   transpose(tL10,tL01);
-
-   Dsp::ThreeMatrix tLI;
-   tLI.initialize(3,3);
-   multiply(tLI,tL01,tL10);
-
-   tL01.show("tL01");
-   tL10.show("tL10");
-   tLI.show("tLI");
-
 }
 
