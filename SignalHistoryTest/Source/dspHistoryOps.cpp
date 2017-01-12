7336828696c94aca80a63f42820c6a15fc1717a4
chrishoen <chris.hoen@red-inc.us>
2017-01-11 19:49:03 -0500

    added history operators

---
+++ SignalHistoryTest/Source/dspHistoryOps.cpp
@@ -0,0 +1,89 @@
+/*==============================================================================
+Description:
+==============================================================================*/
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include "dsp_math.h"
+#include "dspStatistics.h"
+#include "dspHistoryOps.h"
+
+namespace Dsp
+{
+
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Copy X to Y.
+
+bool historyCopy(
+   History& aX,
+   History& aY)
+{
+   // Initialize Y with the size of X.
+   aY.initialize(aX.mNumSamples);
+
+   // Start Y.
+   aY.startHistory();
+
+   // Loop through all of the samples in X.
+   for (int k = 0; k < aX.mNumSamples; k++)
+   {
+      double tTimeX  = 0.0;
+      double tValueX = 0.0;
+      
+      // Get the sample from X.
+      aX.getSampleAtIndex(k,&tTimeX,&tValueX);
+
+      // Put the sample to Y.
+      aY.putSample(tTimeX,tValueX);
+   }
+
+   // Finish Y.
+   aY.finishHistory();
+}
+
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Copy X to Y with a delay.
+
+bool historyCopyWithDelay(
+   History& aX,
+   double   aDelay,
+   History& aY)
+{
+   // Initialize Y with the size of X.
+   aY.initialize(aX.mNumSamples);
+
+   // Start Y.
+   aY.startHistory();
+
+   // Loop through all of the samples in X.
+   for (int k = 0; k < aX.mNumSamples; k++)
+   {
+      double tTimeX  = 0.0;
+      double tValueX = 0.0;
+      
+      // Get the time from X.
+      aX.getTimeAtIndex(k,&tTimeX);
+
+      // Get the value from X, interpolate backward with a delay.
+      aX.getValueInterpolateBefore(k,aDelay,&tValueX);
+
+      // Put the sample to Y.
+      aY.putSample(tTimeX,tValueX);
+   }
+
+   // Finish Y.
+   aY.finishHistory();
+}
+}//namespace
