d428524e566ead71526514730a24e29e71c4a3c6
chrishoen <chris.hoen@red-inc.us>
2017-01-31 12:59:36 -0500

    added dspHistoryTextFile

--- HistoryTest/Source/TestOne_run2.cpp
+++ HistoryTest/Source/TestOne_run2.cpp
@@ -18,6 +18,7 @@ Description:
 #include "dspHistoryGenRandWave.h"
 #include "dspHistoryStatistics.h"
 #include "dspHistoryLoopClock.h"
+#include "dspHistoryTextFile.h"
 
 #include "Parms.h"
 #include "TestOne.h"
@@ -63,27 +64,9 @@ void TestOne::doRun2()
    // Loop to transfer the signal history to an output file.
 
    // Output file.
-   CsvFileWriter  tSampleWriter;
+   HistoryCsvFileWriter  tSampleWriter;
    tSampleWriter.open(gParms.mOutputFile);
-
-
-   // Loop through all of the samples in the history.
-   for (int k=0;k<tHistory.mNumSamples;k++)
-   {
-      // Read the sample from the history.
-      int    tIndex = k;
-      double tTime  = 0.0;
-      double tValue = 0.0;
-      tHistory.readSampleAtIndex(k,&tTime,&tValue);
-
-      // Write the sample to the output file.
-      tSampleWriter.writeRow(
-         tIndex,
-         tTime,
-         tValue);
-   }
-
-   // Close the output file.
+   tSampleWriter.writeHistory(tHistory);
    tSampleWriter.close();
 
    //***************************************************************************
