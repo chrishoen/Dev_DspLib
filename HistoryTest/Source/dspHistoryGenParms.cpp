93bf3e2ce39fbcbde0bd319b95e1506bd14eea7f
chrishoen <chris.hoen@red-inc.us>
2017-01-31 10:38:58 -0500

    dspHistoryGenParms

---
+++ HistoryTest/Source/dspHistoryGenParms.cpp
@@ -0,0 +1,103 @@
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include "my_functions.h"
+#include "risCmdLineFile.h"
+#include "risPortableCalls.h"
+
+#include "dspHistoryGenParms.h"
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+namespace Dsp
+{
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Constructor.
+
+HistoryGenParms::HistoryGenParms()
+{
+   reset();
+}
+
+void HistoryGenParms::reset()
+{
+   BaseClass::reset();
+
+   mDuration = 10.0;
+   mFs = 1.0;
+   mFc = 1.0;
+   mEX = 0.0;
+   mUX = 1.0;
+   mFilterOrder = 4;
+
+   mTs = 1.0 / mFs;
+   mNumSamples = (int)(mDuration * mFs);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Show.
+
+void HistoryGenParms::show(char* aLabel)
+{
+   printf("HistoryGenParms ******** BEGIN %s\n", aLabel);
+
+   printf("Duration           %10.4f\n", mDuration);
+   printf("Fs                 %10.4f\n", mFs);
+   printf("Fc                 %10.4f\n", mFc);
+   printf("FilterOrder        %10d\n", mFilterOrder);
+   printf("EX                 %10.4f\n", mEX);
+   printf("UX                 %10.4f\n", mUX);
+
+   printf("HistoryGenParms ******** END %s\n", aLabel);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Base class override: Execute a command from the command file to set a 
+// member variable.  Only process commands for the target section.This is
+// called by the associated command file object for each command in the file.
+
+void HistoryGenParms::execute(Ris::CmdLineCmd* aCmd)
+{
+   if (aCmd->isCmd("Duration")) mDuration = aCmd->argDouble(1);
+   if (aCmd->isCmd("Fs")) mFs = aCmd->argDouble(1);
+   if (aCmd->isCmd("Fc")) mFc = aCmd->argDouble(1);
+   if (aCmd->isCmd("EX")) mEX = aCmd->argDouble(1);
+   if (aCmd->isCmd("UX")) mUX = aCmd->argDouble(1);
+
+   if (aCmd->isCmd("FilterOrder")) mFilterOrder = aCmd->argInt(1);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Calculate expanded member variables. This is called after the entire
+// section of the command file has been processed.
+
+void HistoryGenParms::expand()
+{
+   mTs = 1.0 / mFs;
+   mNumSamples = (int)(round(mDuration) * mFs);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+}//namespace
---
+++ HistoryTest/Source/dspHistoryGenParms.h
@@ -0,0 +1,97 @@
+#pragma once
+
+/*==============================================================================
+
+Parameters class whose values are read from a command file. 
+
+==============================================================================*/
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+#include "risCmdLineExec.h"
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+namespace Dsp
+{
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// This is a class that contains parameter member variables. The values of
+// the parameters are set by reading a text file that contains command lines.
+// Each command line is of the form "command argument1 argument2 ...".
+// 
+// The command files are partitioned into different sections and only one
+// section can be read at a time to set member variables that are specified
+// in it.
+//
+// The command files are managed by a CmdLineFile object. This opens the 
+// file, reads each line in it, parses the line into a CmdLineCmd command 
+// object, passes the command object to this object for command execution,
+// and then closes the file. 
+//
+// This class inherits from BaseCmdLineParms, which inherits from 
+// BaseCmdLineExec. BaseCmdLineParms provides a method that uses a
+// CmdLineFile object to read and process the file. BaseCmdLineExec provides
+// an abstract execute(cmd) method to which inheritors provide an overload
+// that is called by the CmdLineFile object for each command in the file.
+// This execute method then sets a member variables, according to the
+// command.
+// 
+// This class can contain member variables that also inherit from
+// BaseCmdLineExec. This provides for command files that have a nested
+// structure. If so, then this class is the root.
+// 
+
+class HistoryGenParms : public Ris::BaseCmdLineExec
+{
+public:
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Members that are read from the parms file.
+
+   double  mDuration;     // History duration
+   double  mFs;           // Sampling frequency
+   double  mFc;           // Cutoff frequency 
+   int     mFilterOrder;  // Filter order
+
+   double  mEX;           // Desired expectation
+   double  mUX;           // Desired uncertainty
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Expanded members that are not read from the parms file.
+
+   double  mTs;           // Sampling period
+   int     mNumSamples;   // Number of samples in array
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Infrastucture.
+
+   // Constructor,
+   typedef Ris::BaseCmdLineExec BaseClass;
+   HistoryGenParms();
+   void reset();
+   void show(char* aLabel);
+
+   // Base class override: Execute a command from the command file to set a 
+   // member variable. This is called by the associated command file object
+   // for each command in the file.
+   void execute(Ris::CmdLineCmd* aCmd);
+
+   // Calculate expanded member variables. This is called after the entire
+   // section of the command file has been processed.
+   void expand();
+};
+
+}//namespace
