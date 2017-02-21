f1a63a53757590232490d7eba779d178630f5a1c
chrishoen <chris.hoen@red-inc.us>
2017-02-21 07:49:42 -0500

    history converter

--- DspLib/Source/dspHistoryConverters.cpp
+++ DspLib/Source/dspHistoryConverters.cpp
@@ -31,6 +31,41 @@ void convertHistoryFromType2ToType1(
    double   aFs,
    History& aOutput)
 {
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Initialize the output history.
+
+   // Calculate the number of samples needed to create the output history.
+   int tMaxSamples = (int)(aInput.mDuration * aFs);
+
+   // Calculate the samppling period.
+   double tTs = 1.0/aFs;
+
+   // Initialize the output history.
+   aOutput.initialize(tMaxSamples);
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Loop to transfer the input history to the output history.
+
+   // Loop time 
+   double tLoopTime = 0.0;
+
+   // Loop through all of the samples in the history.
+   for (int k = 0; k < aOutput.mMaxSamples; k++)
+   {
+      // Get the value from the input history at the loop time. This does an
+      // interpolation.
+      double tValue = aInput.readValueAtTime(tLoopTime);
+      // Set the time and value of the output history to the loop time and 
+      // the input value.
+      aOutput.mTime[k]  = tLoopTime;
+      aOutput.mValue[k] = tValue;
+      // Advance the loop time by the sampling period.
+      tLoopTime += tTs;
+   }
 }
 
 //******************************************************************************
