e02755579247ea1e7240f4055a9f9a2e1ed5f740
chrishoen <chris.hoen@red-inc.us>
2017-09-30 21:45:55 -0400

    add mNumSteps to RootFinder

--- DspLib/Source/dspRootFinder.cpp
+++ DspLib/Source/dspRootFinder.cpp
@@ -18,84 +18,85 @@ Description:
 namespace Dsp
 {
 
-   //******************************************************************************
-   //******************************************************************************
-   //******************************************************************************
-   // This finds the roots of a given function, starting at an initial guess.
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// This finds the roots of a given function, starting at an initial guess.
 
-   bool RootFinder::findRoot(
-      BaseFunctionObject* aFunctionObject,      // Input
-      Eigen::VectorXd&    aXInitial,            // Input
-      Eigen::VectorXd&    aWeight,              // Input
-      double              aAccuracy,            // input        
-      int                 aMaxSteps,            // input        
-      Eigen::VectorXd&    aX)                   // Output
+bool RootFinder::findRoot(
+   BaseFunctionObject* aFunctionObject,      // Input
+   Eigen::VectorXd&    aXInitial,            // Input
+   Eigen::VectorXd&    aWeight,              // Input
+   double              aAccuracy,            // input        
+   int                 aMaxSteps,            // input        
+   Eigen::VectorXd&    aX)                   // Output
+{
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Initialize.
+
+   // Initialize the output matrix.
+   int tDim = aFunctionObject->dimension();
+   aX = Eigen::VectorXd(tDim);
+
+   // Temp independent variable matrix.
+   Eigen::VectorXd tXs(tDim);
+
+   // Temp dependent variable matrix.
+   Eigen::VectorXd tY(tDim);
+
+   // Temp jacobian matrix.
+   Eigen::MatrixXd tJ(tDim, tDim);
+
+	// Temp jacobian inverse matrix.
+	Eigen::MatrixXd tJinv(tDim, tDim);
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Loop.
+
+   aX = aXInitial;
+
+   bool tSuccess = false;
+   mNumSteps = 0;
+
+   while (true)
    {
-      //***************************************************************************
-      //***************************************************************************
-      //***************************************************************************
-      // Initialize.
+      // Evaluate the function and its jacobian at the current
+      // iteration value
+      aFunctionObject->evaluateFunction(aX, tY);
+      aFunctionObject->evaluateJacobian(aX, tJ);
 
-      // Initialize the output matrix.
-      int tDim = aFunctionObject->dimension();
-      aX = Eigen::VectorXd(tDim);
+      // Calculate the newton raphson algorithm.
+      // change to psuedo inverse
+      tJinv = tJ.completeOrthogonalDecomposition().pseudoInverse();
+      tXs = aX - tJinv*tY;
 
-      // Temp independent variable matrix.
-      Eigen::VectorXd tXs(tDim);
+      // Caclulate the difference between the current and previous
+      // iterations.
+      double tDiff = (tXs - aX).norm();
 
-      // Temp dependent variable matrix.
-      Eigen::VectorXd tY(tDim);
+      // Store the current iteration value.
+      aX = tXs;
 
-      // Temp jacobian matrix.
-      Eigen::MatrixXd tJ(tDim, tDim);
-
-	  // Temp jacobian inverse matrix.
-	  Eigen::MatrixXd tJinv(tDim, tDim);
-
-      //***************************************************************************
-      //***************************************************************************
-      //***************************************************************************
-      // Loop.
-
-      aX = aXInitial;
-
-      bool tSuccess = false;
-      int tCount = 0;
-
-      while (true)
+      // Test the difference.
+      if (tDiff < aAccuracy)
       {
-         // Evaluate the function and its jacobian at the current
-         // iteration value
-         aFunctionObject->evaluateFunction(aX, tY);
-         aFunctionObject->evaluateJacobian(aX, tJ);
+         tSuccess = true;
+         break;
+      }
 
-         // Calculate the newton raphson algorithm.
-         // change to psuedo inverse
-		 tJinv = tJ.completeOrthogonalDecomposition().pseudoInverse();
-         tXs = aX - tJinv*tY;
+      // Test the number of iteration steps.
+      if (++mNumSteps > aMaxSteps)
+      {
+         tSuccess = false;
+         break;
+      }
+   } 
 
-         // Caclulate the difference between the current and previous
-         // iterations.
-         double tDiff = (tXs - aX).norm();
-
-         // Store the current iteration value.
-         aX = tXs;
-
-         // Test the difference.
-         if (tDiff < aAccuracy)
-         {
-            tSuccess = true;
-            break;
-         }
-
-         // Test the number of iteration steps.
-         if (++tCount > aMaxSteps)
-         {
-            tSuccess = false;
-            break;
-         }
-      } 
-
+   // Done.
    return tSuccess;      
 }
 
