80d8c37ae06f93d46e53b4de5b58578bf2b5b8f6
chrishoen <chris123hoen@gmail.com>
2019-04-03 21:52:13 -0400

    new cmake pch

--- MyCodeMacros.cmake
+++ MyCodeMacros.cmake
@@ -12,8 +12,9 @@ function(my_find_src_files _a_src_files _target)
       list(FILTER _src_files EXCLUDE REGEX ".*_win.cpp$")
    endif()
 
-   set(${_a_src_files} ${_src_files} PARENT_SCOPE)
+#   list(APPEND _src_files "stdafx.h")
 
+   set(${_a_src_files} ${_src_files} PARENT_SCOPE)
 
    message(STATUS "***********************************************************" ${_target})
    message(STATUS "my_find_src_files************************BEGIN " ${_target})
@@ -83,42 +84,6 @@ function(my_add_compile_options _target)
       target_compile_definitions(${_target} PRIVATE "-DNDEBUG")
    endif()
 
-endfunction()
-
-#*******************************************************************************
-#*******************************************************************************
-#*******************************************************************************
-
-function(my_add_pch _target)
-   if (NOT MSVC)
-      return()
-   endif()
-
-   message(STATUS "my_add_pch************************************ " ${_target})
-
-   get_target_property(_src_files      ${_target} SOURCES)
-   get_target_property(_tgt_binary_dir ${_target} BINARY_DIR)
-   get_target_property(_tgt_name       ${_target} NAME)
-
-   #message(STATUS "my_add_pch PROJECT_SOURCE_DIR***********" ${PROJECT_SOURCE_DIR})
-   #message(STATUS "my_add_pch PROJECT_BINARY_DIR***********" ${PROJECT_BINARY_DIR})
-   #message(STATUS "my_add_pch _src_files*******************" ${_src_files})
-   #message(STATUS "my_add_pch _tgt_binary_dir**************" ${_tgt_binary_dir})
-   #message(STATUS "my_add_pch _tgt_name********************" ${_tgt_name})
-
-   foreach(_src_file ${_src_files})
-      if(${_src_file} MATCHES "stdafx.cpp") 
-         #message(STATUS "_src_fileEQ*******************" ${_src_file})
-         set_source_files_properties(${_src_file} PROPERTIES
-         COMPILE_FLAGS "/Yc\"stdafx.h\" /Fp\"${_tgt_binary_dir}\\${_tgt_name}.pch\""
-         OBJECT_OUTPUTS "${_tgt_binary_dir}\\${_tgt_name}.pch")
-      else()
-         #message(STATUS "_src_fileNE*******************" ${_src_file})
-         set_source_files_properties(${_src_file} PROPERTIES
-         COMPILE_FLAGS "/Yu\"stdafx.h\" /Fp\"${_tgt_binary_dir}\\${_tgt_name}.pch\""
-         OBJECT_DEPENDS "${_tgt_binary_dir}\\${_tgt_name}.pch")
-      endif()
-   endforeach()
 endfunction()
 
 #*******************************************************************************
