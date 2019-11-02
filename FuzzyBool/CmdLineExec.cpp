55913c3310df3cb143daf02eb4918f23a12b9fa4
chrishoen <chris123hoen@gmail.com>
2019-11-01 23:43:56 -0400

    more FuzzyBool

--- FuzzyBool/CmdLineExec.cpp
+++ FuzzyBool/CmdLineExec.cpp
@@ -42,10 +42,17 @@ void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
 
    Dsp::FuzzyBool tY1 = ~tX2;
    Dsp::FuzzyBool tY2 = tX1 & tX2;
-   Dsp::FuzzyBool tY3 = tX1 | tX2;;
+   Dsp::FuzzyBool tY3 = tX1 | tX2;
+   Dsp::FuzzyBool tY4 = tX1 & ~tX2;
 
-   Prn::print(0,"%5.3f %5.3f",      tX1.mX,tX2.mX);
-   Prn::print(0,"%5.3f %5.3f %5.3f",tY1.mX,tY2.mX,tY3.mX);
+   Prn::print(0, "X1 %5.3f", tX1.mX);
+   Prn::print(0, "X2 %5.3f", tX2.mX);
+   Prn::print(0, "");
+
+   Prn::print(0, "Y1 %5.3f", tY1.mX);
+   Prn::print(0, "Y2 %5.3f", tY2.mX);
+   Prn::print(0, "Y3 %5.3f", tY3.mX);
+   Prn::print(0, "Y4 %5.3f", tY4.mX);
 }
 
 //******************************************************************************
@@ -82,6 +89,19 @@ void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
 
 void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
 {
+   aCmd->setArgDefault(1, 0.90);
+   aCmd->setArgDefault(2, 0.80);
+
+   Dsp::FuzzyBool tX1(aCmd->argDouble(1));
+   Dsp::FuzzyBool tX2(aCmd->argDouble(2));
+
+   Dsp::FuzzyBool tY1 = tX1 & ~tX2;
+   bool tC1 = (tY1 = tX1 & ~tX2).crisp(0.5);
+
+   Prn::print(0, "X1 %5.3f", tX1.mX);
+   Prn::print(0, "X2 %5.3f", tX2.mX);
+   Prn::print(0, "Y1 %5.3f", tY1.mX);
+   Prn::print(0, "C1 %5s", my_string_from_bool(tC1));
 }
 
 //******************************************************************************
