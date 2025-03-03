bb417ff916e2604350179525c17901535d0840e6
chrishoen <chris123hoen@gmail.com>
2025-03-03 11:51:15 -0500

    moved periodic statistics

--- DspLib/dspPeriodicStatistics.h
+++ DspLib/dspPeriodicStatistics.h
@@ -9,21 +9,18 @@ filters
 //******************************************************************************
 //******************************************************************************
 
-#include "dspFilterAlpha.h"
-#include "dspFilterFilters.h"
 
 namespace Dsp
 {
-namespace Filter
-{
-
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
 // This filter characterizes an input signal by calculating expectations and
-// uncertainties of the signal. It does this with a set of alpha filters.
+// uncertainties of the signal. It does this on a periodic basis, given a fixed
+// window size. For example, given a fixed window size of 1000, and an update
+// rate of 1000 khz, this will produce new statisics once a second.
 
-class AlphaStatistics
+class PeriodicStatistics
 {
 public:
    //***************************************************************************
@@ -31,41 +28,49 @@ public:
    //***************************************************************************
    // Members.
 
-   // Output values.
-   double mX;          // Input value
-   double mEX;         // Expectation (mean)
-   double mUX;         // Uncertainty (standard deviation)
-   double mVariance;   // Variance
-   double mMean;       // Expectation (mean)
-   double mStdDev;     // Uncertainty (standard deviation)
+   // Members.
+   int mSize;
+   bool mFirstFlag;
 
-   // Alpha filters for expectation and uncertainty.
-   AlphaOne    mXAlpha;        // Alpha filter for input X
-   AlphaOne    mXSquareAlpha;  // Alpha filter for input X squared
+   // Output values.
+   bool   mEndOfPeriod;  // True at the end of a period
+   float  mX;            // Input value
+   float  mEX;           // expectation (mean)
+   float  mUX;           // uncertainty (standard deviation)
+   float  mMinX;         // minimum 
+   float  mMaxX;         // maximum
+   float  mMean;         // expectation (mean)
+   float  mStdDev;       // uncertainty (standard deviation)
+
+   float  mEXSquare;     // expectation of X squared
+   float  mVariance;     // variance
+
+   // Sums for expectation and uncertainty.
+   float  mXSum;         // Sum of X
+   float  mXSquareSum;   // Sum of X squared
+   int    mPutCount;     // Put count
+
+   // Current min and max.
+   float  mCurrentMinX;  // minimum
+   float  mCurrentMaxX;  // maximum
 
    // Counter.
-   int  mK;
+   int mK;
 
    //***************************************************************************
    //***************************************************************************
    //***************************************************************************
    // Methods.
 
-   // Constructor.
-   AlphaStatistics();
-
    // Initialize.
-   void initialize(double aAlpha);
-
-   // Put input value
-   void put(double aX);
-
-   // Helpers.
+   void initialize(int aSize);
    void show();
+
+   // Put input value.
+   void put(float aX);
 };
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
-}//namespace
 }//namespace
