8223346c524beccaa6b506f97de51415ba767099
chrishoen <chris123hoen@gmail.com>
2025-03-03 18:15:04 -0500

    renamed to dspFilterDevAlpha for StdDevAlpha filter

--- DspLib/dspFilterDevAlpha.cpp
+++ DspLib/dspFilterDevAlpha.cpp
@@ -8,7 +8,7 @@ Description:
 
 #include "stdafx.h"
 
-#include "dspFilterStatistics.h"
+#include "dspFilterDevAlpha.h"
 
 namespace Dsp
 {
@@ -22,9 +22,13 @@ namespace Filter
 //******************************************************************************
 //******************************************************************************
 
-AlphaStatistics::AlphaStatistics()
+StdDevAlpha::StdDevAlpha()
 {
-   // Output values
+   resetVars();
+}
+
+void StdDevAlpha::resetVars()
+{
    mX = 0.0;
    mEX = 0.0;
    mUX = 0.0;
@@ -34,36 +38,56 @@ AlphaStatistics::AlphaStatistics()
    mK = 0;
 }
 
-void AlphaStatistics::initialize(double aAlpha)
+// Initialize from alpha.
+void StdDevAlpha::initializeFromAlpha (double aAlpha)
 {
-   mXAlpha.initialize(aAlpha);
-   mXSquareAlpha.initialize(aAlpha);
-   mX = 0.0;
-   mEX = 0.0;
-   mUX = 0.0;
-   mVariance = 0.0;
-   mMean   = 0.0;
-   mStdDev = 0.0;
-   mK = 0;
+   mXAlpha.initializeFromAlpha(aAlpha);
+   mXSquareAlpha.initializeFromAlpha(aAlpha);
+   resetVars();
+}
+
+// Initialize from lambda (tracking index).
+void StdDevAlpha::initializeFromLambda (double aLambda)
+{
+   mXAlpha.initializeFromLambda(aLambda);
+   mXSquareAlpha.initializeFromLambda(aLambda);
+   resetVars();
+
+}
+
+// Initialize from step response time and threshold.
+void StdDevAlpha::initializeFromStep(double aTs, double aStepTime, double aStepThresh)
+{
+   mXAlpha.initializeFromStep(aTs, aStepTime, aStepThresh);
+   mXSquareAlpha.initializeFromStep(aTs, aStepTime, aStepThresh);
+   resetVars();
+
+}
+// Set the first flag true.
+void StdDevAlpha::setFirst()
+{
+   mXAlpha.setFirst();
+   mXSquareAlpha.setFirst();
 }
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
 
-void AlphaStatistics::put(double aX)
+void StdDevAlpha::put(double aX)
 {
-   // These result in moving averages of X and X squared
+   // These give moving averages (moving expectatons) of X and X squared.
+   // E[X] and E[X^2].
    mXAlpha.put(aX);
    mXSquareAlpha.put(aX*aX);
 
    // X
    mX = aX;
 
-   // Expectation (mean) of X
+   // Expectation (mean) of X is E[X]
    mEX = mXAlpha.mXX;
 
-   // Variance of X
+   // Variance of X is E[X^2] - E[X]^2
    mVariance = mXSquareAlpha.mXX - mEX*mEX;
 
    // Uncertainty (stddev) of X
@@ -88,7 +112,7 @@ void AlphaStatistics::put(double aX)
 //******************************************************************************
 //******************************************************************************
 
-void AlphaStatistics::show()
+void StdDevAlpha::show()
 {
    printf("%3d %8.3f %8.3f %8.3f\n",
       mK,
--- DspLib/dspFilterDevAlpha.h
+++ DspLib/dspFilterDevAlpha.h
@@ -22,8 +22,9 @@ namespace Filter
 //******************************************************************************
 // This filter characterizes an input signal by calculating expectations and
 // uncertainties of the signal. It does this with a set of alpha filters.
+// The expectation and uncertainty calculated are mean and standard deviation.
 
-class AlphaStatistics
+class StdDevAlpha
 {
 public:
    //***************************************************************************
@@ -40,8 +41,8 @@ public:
    double mStdDev;     // Uncertainty (standard deviation)
 
    // Alpha filters for expectation and uncertainty.
-   AlphaOne    mXAlpha;        // Alpha filter for input X
-   AlphaOne    mXSquareAlpha;  // Alpha filter for input X squared
+   AlphaOne mXAlpha;        // Alpha filter for input X
+   AlphaOne mXSquareAlpha;  // Alpha filter for input X squared
 
    // Counter.
    int  mK;
@@ -52,12 +53,21 @@ public:
    // Methods.
 
    // Constructor.
-   AlphaStatistics();
+   StdDevAlpha();
+   void resetVars();
 
-   // Initialize.
-   void initialize(double aAlpha);
+   // Initialize with lambda (tracking index).
+   void initialize (double aLambda); 
+   // Initialize with alpha.
+   void initializeFromAlpha (double aP1); 
+   // Initialize with lambda (tracking index).
+   void initializeFromLambda (double aLambda); 
+   // Initialize from step response time and threshold.
+   void initializeFromStep(double aTs, double aStepTime, double aStepThresh);
+   // Set the first flag true.
+   void setFirst(); 
 
-   // Put input value
+   // Put input value to calculate output variable results.
    void put(double aX);
 
    // Helpers.
